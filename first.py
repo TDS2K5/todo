4ca29a4a829a4305e823eab38ee1773cea6ffc57
TDS2K5 <berlinryuk@gmail.com>
2025-07-26 12:49:04 +0530

    added todo

--- first.py
+++ first.py
@@ -1,15 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+db=SQLAlchemy(app)
+    
+class Todo(db.Model):
+    sno=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(200), nullable=False)
+    desc=db.Column(db.String(500), nullable=False)
+    date=db.Column(db.DateTime,default=datetime.utcnow)
 
-@app.route("/")
+    def __repr__(self):
+        return  f"{self.sno} - {self.title}"
+with app.app_context():
+    db.create_all()
+
+@app.route("/", methods=['GET','post'])
 def hello_world():
-    return render_template("index.html")
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo=Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
 
-@app.route("/first")
+    allTodo=Todo.query.all()    
+    return render_template("index.html", allTodo=allTodo)
+
+@app.route("/show")
 def first():
+    allTodo=Todo.query.all()
+    print(allTodo)
     return render_template("first.html")
 
-app.run(debug=True)
+@app.route("/delete/<int:sno>")
+def delt(sno):
+    todo=Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+@app.route("/update")
+def up():
+    allTodo=Todo.query.all()
+    print(allTodo)
+    return redirect('/')
+
+if __name__== "__main__":
+    app.run(debug=True)
